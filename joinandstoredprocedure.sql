1431f25cfd8f2653e759d002c349a94917564219
jaydeep <jaydeep.vaghela@aimdek.com>
2022-01-19 18:32:39 +0530

    joinandstoredprocedure.sql file is updated

--- joinandstoredprocedure.sql
+++ joinandstoredprocedure.sql
@@ -1,11 +1,12 @@
+/* First Table : Employee */
 
-/*JOIN QUERY FOR THREE TABLES*/
-
-CREATE TABLE EmpInformation (
+CREATE TABLE Employee
+(
 	emp_id int primary key,
-	emp_name varchar(20));
+	emp_name varchar(20)
+);
 
-INSERT INTO EmpInformation VALUES
+INSERT INTO Employee VALUES
 (01, 'Jaydeep'),
 (02, 'Viraj'),
 (03, 'Mohan'),
@@ -13,15 +14,17 @@ INSERT INTO EmpInformation VALUES
 (05, 'Rahul'),
 (06, 'Amrut');
 
-SELECT * FROM EmpInformation;
+SELECT * FROM Employee;
 
-CREATE TABLE RESULT (
+/* Second Table : Organization */
+
+CREATE TABLE Organization (
 	company_id int primary key, 
 	emp_id int, 
     points int, 
 	status varchar(20));
 
-INSERT INTO RESULT VALUES
+INSERT INTO Organization VALUES
 (101, 1, 73, 'PASS'),
 (102, 3, 72, 'PASS'),
 (103, 5, 74, 'PASS'),
@@ -33,66 +36,67 @@ INSERT INTO RESULT VALUES
 (109, 6, 68, 'PASS'),
 (110, 4, 98, 'PASS');
 
-SELECT * FROM RESULT;
+SELECT * FROM Organization;
 
-CREATE TABLE AllInformation (
+/* Third Table : Profile */
+
+CREATE TABLE Profile (
 	city varchar(20), 
 	email varchar(20), 
     company_id int, 
 	post varchar(50));
 
-INSERT INTO AllInformation VALUES 
+INSERT INTO Profile VALUES 
 ('Delhi', 'mohan@gmail.com', 102, 'Trainee Software Developer'),
 ('Mumbai', 'jaydeep@gmail.com', 101, 'Business Analyst'),
 ('Kolkata', 'rahul@gmail.com', 103, 'Laravel Developer'),
 ('Chennai', 'viraj@gmail.com', 104, 'Salesforce Developer');
 
-/* INNER JOIN */
+/* The INNER JOIN keyword selects all rows from both the tables as long as the condition satisfies.  */
 SELECT emp_name, points, status, city, email,
-post from EmpInformation e INNER JOIN Result r on
-e.emp_id = r.emp_id INNER JOIN AllInformation a on 
+post from Employee e INNER JOIN Organization r on
+e.emp_id = r.emp_id INNER JOIN Profile a on 
 a.company_id = r.company_id;
 
-/* FULL OUTER JOIN */
+/* FULL JOIN creates the result-set by combining result of both LEFT JOIN and RIGHT JOIN. */
 SELECT emp_name, points, status, city, email,
-post from EmpInformation e FULL OUTER JOIN Result r on
-e.emp_id = r.emp_id FULL OUTER JOIN AllInformation a on 
+post from Employee e FULL OUTER JOIN Organization r on
+e.emp_id = r.emp_id FULL OUTER JOIN Profile a on 
 a.company_id = r.company_id;
 
-/* LEFT JOIN */
+/* This join returns all the rows of the table on the left side of the join and matching rows for the table on the right side of join. */
 SELECT emp_name, points, status, city, email,
-post from EmpInformation e LEFT JOIN Result r on
-e.emp_id = r.emp_id LEFT JOIN AllInformation a on 
+post from Employee e LEFT JOIN Organization r on
+e.emp_id = r.emp_id LEFT JOIN Profile a on 
 a.company_id = r.company_id;
 
-/* RIGHT JOIN */
+/* This join returns all the rows of the table on the right side of the join and matching rows for the table on the left side of join. */
 SELECT emp_name, points, status, city, email,
-post from EmpInformation e RIGHT JOIN Result r on
-e.emp_id = r.emp_id RIGHT JOIN AllInformation a on 
+post from Employee e RIGHT JOIN Organization r on
+e.emp_id = r.emp_id RIGHT JOIN Profile a on 
 a.company_id = r.company_id;
 
-/* STORED PROCEDURE */
+/* A stored procedure is a prepared SQL code that  can be reused over and over again. */
 
-CREATE PROCEDURE EmpData
+CREATE PROCEDURE CommonProcedure
 AS
-SELECT * FROM AllInformation
+SELECT * FROM Profile
 GO;
 
-EXEC EmpData;
+EXEC CommonProcedure;
 
-/* STORED PROCEDURE WITH SINGLE PARAMETER */
+/* Stored procedure with single parameter */
 
 CREATE PROCEDURE EmpSinglePara @city nvarchar(30)
 AS
-SELECT * FROM AllInformation WHERE city = @city;
-
+SELECT * FROM Profile WHERE city = @city;
 
 EXEC EmpSinglePara @city = 'Delhi';
 
-/* STORED PROCEDURE WITH MULTIPLE PARAMETER */
+/* Stored procedure with multiple parameter */
+
 CREATE PROCEDURE EmpMultipleParameter @city nvarchar(30), @post varchar(20)
 AS
-SELECT * FROM AllInformation WHERE city = @city AND post = @post;
+SELECT * FROM Profile WHERE city = @city AND post = @post;
 
 EXEC EmpMultipleParameter @city = 'Chennai', @post = 'Salesforce Developer';
-
